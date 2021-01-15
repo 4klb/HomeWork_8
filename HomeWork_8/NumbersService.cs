0e099c416ad232d4bfc9f73a4838ef7fc9060a23
Hadz_xt61@student.itstep.kz <Hadz_xt61@student.itstep.kz>
2021-01-15 22:30:10 +0600

    Добавьте файлы проекта.

---
+++ HomeWork_8/NumbersService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_8
+{
+    class NumbersService
+    {
+        private bool flag = true;
+
+        public int NumbersDivider(string firstNumber, string secondNumber)
+        {
+            while (flag)
+            {
+                IsNumberCorrect(firstNumber, secondNumber);
+            }
+            int.TryParse(firstNumber, out int firstCorrectNumber);
+            int.TryParse(secondNumber, out int secondCorrectNumber);
+
+            return firstCorrectNumber / secondCorrectNumber;
+        }
+
+        public string IsNumberCorrect(string firstNumber, string secondNumber)
+        { 
+            if (firstNumber == string.Empty || secondNumber == string.Empty)
+            {
+                throw new ArgumentNullException("Нужно ввести число");
+            }
+            else if (int.TryParse(firstNumber, out int correctFirstNumber) && correctFirstNumber == 0)
+            {
+                throw new ArgumentException("Делить на ноль нельзя");
+            }
+            else if(int.TryParse(secondNumber, out int correctSecondNumber) && correctSecondNumber == 0)
+            {
+                throw new ArgumentException("Делить на ноль нельзя");
+            }
+            else
+            {
+                flag = false;
+                NumbersDivider(firstNumber,secondNumber);
+                return "0";
+            }
+        }
+    }
+}
---
+++ HomeWork_8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace HomeWork_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            var numbersService = new NumbersService();
+            try
+            {
+                Console.WriteLine("Введите два числа(деление)");
+                string firstIncomingNumber = Console.ReadLine();
+                string secondIncomingNumber = Console.ReadLine();
+                int result = numbersService.NumbersDivider(firstIncomingNumber, secondIncomingNumber);
+                Console.WriteLine(result);
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            Console.WriteLine();
+
+            //Задание 2
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            try
+            {
+                for (int i = 0; i < array.Length + 1; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Обработка массива завершена");
+            }
+        }
+    }
+}
+
